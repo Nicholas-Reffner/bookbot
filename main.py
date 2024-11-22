694ccc1b6e76dc06fa7bbb6e939a3cc1d70d87bb
Nicholas-Reffner <nick.reffner@gmail.com>
2024-11-21 21:15:05 -0600

    Long night, need to figure out how to get key-value from (total_chars_int) dict into an ordered list.

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    total = total_words(text)
-    print(text)
-    print(total)
+    #calls main book open/read func
 
+    total_words_int = get_total_words(text)
+    #calls count of all words from text file
+
+    total_chars_int = get_character_count(text)
+    #calls count of all characters in text_file
+    
+    sorted_chars_int = sort_character_count(total_chars_int)
+    # takes total chars func and sorts by int
+    print(f"\n{text}")
+    print(f"\n{total_words_int} words found in document")
+    print(f"\n{total_chars_int}")
+    print
+
+
+def get_total_words(txt_file):
+    seperate = txt_file.split()
+    count = len(seperate)
+    return count
+
+
+def get_character_count(text_file):
+    char_count_total = {}
+    text_file_lower = text_file.lower()
+    letters = list(text_file_lower)
+    for i in letters:
+        if i not in char_count_total:
+            char_count_total[i] = 1
+        else:
+            char_count_total[i] += 1
+    return char_count_total
+
+#func takes get_character count and sorts
+def sort_character_count(char_count_total):
+    sorted = char_count_total.sort
+    
+    
+    return sorted
+    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def total_words(txt_file):
-    seperate = txt_file.split()
-    count = len(seperate)
-    return count
-    
-
-
 
 main()
