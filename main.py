2a3b2a8b93d4e62e1bfedb325e0d0f15684a43a9
Nicholas-Reffner <nick.reffner@gmail.com>
2024-11-22 21:29:58 -0600

    Next day, finished the character sorting function and added a function to print a report upon run.

--- main.py
+++ main.py
@@ -11,10 +11,27 @@ def main():
     
     sorted_chars_int = sort_character_count(total_chars_int)
     # takes total chars func and sorts by int
-    print(f"\n{text}")
-    print(f"\n{total_words_int} words found in document")
-    print(f"\n{total_chars_int}")
-    print
+    output_message = output_message_function(sorted_chars_int)
+    
+
+    
+    #print(f"\n{text}")
+    ##print(f"\n{total_chars_int}")
+    #prints sorted_char return
+    #print(sorted_chars_int)
+
+
+
+    print(f" --- Begin report of {book_path} --- ")
+    print(f"{total_words_int} words found in the document")
+    for char in output_message:
+        print(char)
+    #print(list(output_message))
+    print(" --- End report --- ")
+
+
+
+
 
 
 def get_total_words(txt_file):
@@ -22,11 +39,20 @@ def get_total_words(txt_file):
     count = len(seperate)
     return count
 
-
+#takes full text from above function
+#all characters made lowercase
+#characters split into indiviual strings
+#alphabet characters added to letters list
+#iterate though all in letters and adds count to-
+#- char_count_total dictionary
 def get_character_count(text_file):
     char_count_total = {}
+    letters = []
     text_file_lower = text_file.lower()
-    letters = list(text_file_lower)
+    characters = list(text_file_lower)
+    for char in characters:
+        if char.isalpha():
+            letters.append(char) 
     for i in letters:
         if i not in char_count_total:
             char_count_total[i] = 1
@@ -34,12 +60,43 @@ def get_character_count(text_file):
             char_count_total[i] += 1
     return char_count_total
 
-#func takes total_chars_int and sorts
+
+
+#takes results from above dictionary
+#iterates though dict
+#splits key-value tuples to items list
+#appends individual single key-values lists to list_2d
+#sorts list_2d by values in reverse order
 def sort_character_count(total_chars_int):
-    sorted = total_chars_int.sort
+    items = []
+    list_2d = []
+    #list(total_chars_int)
+    for key, value in total_chars_int.items():
+        items.append(key)
+        items.append(value)
+        list_2d.append(items[:2])
+        items.clear()
+    list_2d_sorted = sorted(list_2d, key=lambda x: x[1], reverse=True)
     
+    return list_2d_sorted
+
+        
+
+
+
+def output_message_function(sorted_character_count):
+    char_list = []
+    for char in sorted_character_count:
+        char_list.append(f"the '{char[0]}' character was found {char[1]} times")
     
-    return sorted
+    return char_list
+        
+        
+
+
+
+
+
     
 
 def get_book_text(path):
